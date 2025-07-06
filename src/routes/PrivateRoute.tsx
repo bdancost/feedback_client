c3dad18e313db437df105a8ca9b9334466151a17
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-07-06 20:09:49 -0300

    fix: corrigido erro de namespace JSX no PrivateRoute.tsx e ajustado tsconfig

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import type { JSX } from "react";
 
 type Props = {
   children: JSX.Element;
