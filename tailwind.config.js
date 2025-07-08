4798888a1c4f1d3c571127b124f71bccbdecee1e
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-07-08 11:48:53 -0300

    Corrigindo erros

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // aqui diz para olhar todos os arquivos JS/TS/JSX/TSX na pasta src
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#4f46e5", // azul-indigo
-        secondary: "#ec4899", // rosa
-        accent: "#fbbf24", // amarelo
+        primary: "#4f46e5",
+        secondary: "#ec4899",
+        accent: "#fbbf24",
+      },
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+        heading: ["Poppins", "ui-sans-serif", "system-ui"],
       },
     },
   },
